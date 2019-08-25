f968057b1a9a531bfaa65395575fb0090ac058f1
Aditya Sreepad <aditya.g.sreepad@gmail.com>
2019-08-26 00:46:00 +0530

    Heroku - try 3

--- NJF/settings.py
+++ NJF/settings.py
@@ -16,12 +16,12 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
-if DEBUG:
-   STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static'),
-   ]
-else:
-   STATIC_ROOT = os.path.join(BASE_DIR,'static')
+#if DEBUG:
+#   STATICFILES_DIRS = [
+#   os.path.join(BASE_DIR, 'static'),
+#   ]
+#else:
+#   STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -130,4 +130,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
